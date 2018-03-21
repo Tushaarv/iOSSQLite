7ced79446d5567717950a07d15d7a2a4b51e7dd4
Tushar Vengurlekar <tushaarv@gmail.com>
2018-03-21 11:30:12 +0530

    Initial Commit

---
+++ iOSSqlite/AppDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.swift
+//  iOSSqlite
+//
+//  Created by Tushar Vengurlekar on 21/03/18.
+//  Copyright © 2018 Tushaar's. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+}
+
---
+++ iOSSqlite/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  iOSSqlite
+//
+//  Created by Tushar Vengurlekar on 21/03/18.
+//  Copyright © 2018 Tushaar's. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
